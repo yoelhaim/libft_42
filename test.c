0d30299e0d00e7b8650cf44ce0ddba1394f351e2
Youssef Elhaimer <yoelhaim@e1r10p1.1337.ma>
2021-11-01 16:33:27 +0100

    rename files

--- test.c
+++ test.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 //#include "ft_isdigit.c"
-#include "ft_isalnum.c"
-#include "ft_isascii.c"
-#include "ft_isprint.c"
-#include "ft_strlen.c"
-#include "ft_toupper.c"
-#include "ft_tolower.c"
+#include "isalnum.c"
+#include "isascii.c"
+#include "isprint.c"
+#include "strlen.c"
+#include "toupper.c"
+#include "tolower.c"
+#include "atoi.c"
 int main(int ac, char **av)
 {
 
-	printf("%c", ft_tolower('$'));
-	//printf("%d", ft_strlen(av[1]));
+	//printf("%c", ft_tolower('$'));
+//	printf("%d", ft_atoi(av[1]));
+  printf("%s", strrchr(av[1], 'a'));
 //	printf("%d", ft_isprint(127));
 
 
