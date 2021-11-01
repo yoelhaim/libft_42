23d224608166011b47d1d385f40d5f4bb8d97d2d
Youssef Elhaimer <yoelhaim@e1r10p1.1337.ma>
2021-11-01 12:53:28 +0100

    first upload c in 42

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+//#include "ft_isdigit.c"
+#include "ft_isalnum.c"
+#include "ft_isascii.c"
+#include "ft_isprint.c"
+#include "ft_strlen.c"
+#include "ft_toupper.c"
+#include "ft_tolower.c"
+int main(int ac, char **av)
+{
+
+	printf("%c", ft_tolower(70));
+	//printf("%d", ft_strlen(av[1]));
+//	printf("%d", ft_isprint(127));
+
+
+
+}
