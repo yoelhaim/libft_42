93a012cd0aaf0931044e3e85286fa7fdcc538228
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-13 13:16:19 +0100

    finish  ls new

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:28:37 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/10 10:47:31 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/11/13 13:14:28 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,3 @@ t_list	*ft_lstnew(void *content)
 	ls_new->next = NULL;
 	return (ls_new);
 }
-// int		main(void)
-// {
-
-// 	t_list *head;
-
-// 	head = ft_lstnew("awbx");
-// 	head->next = ft_lstnew("king");
-// 	head->next->next = ft_lstnew("was");
-// 	head->next->next->next = ft_lstnew("here");
-
-// 	while (head)
-// 	{
-// 		 printf("head => %s , adress => %p\n", head->content, head);
-// 		head = head->next;
-// 	}
-// }
