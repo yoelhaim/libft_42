811b65b009bbd12039b088ed16d4127a1a934665
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-15 22:58:12 +0100

    fixed error

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,23 +6,23 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 18:20:34 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/05 12:08:57 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/11/15 21:26:14 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *str, int c)
 {
-	size_t	len;
+	size_t	i;
+	char	*temp;
 
-	len = ft_strlen(s);
-	if (c == '\0')
-		return ((char *)s + len);
-	while (len--)
-	{
-		if (s[len] == c)
-			return ((char *)s + len);
-	}
-	return (NULL);
+	temp = NULL;
+	i = 0;
+	while (str[i])
+		if (str[i++] == (char) c)
+			temp = (char *)str + i - 1;
+	if (!c)
+		return ((char *)str + i);
+	return (temp);
 }
